00ac5ae968cd3acbb6c8b65ce6175e1b5657cd85
Shen-Shen <wushenshen@gmail.com>
2018-05-21 17:21:06 -0700

    Lobby page works sometimes

---
+++ client/components/UserList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class UserList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>This is a user list.</div>
+    )
+  }
+}
+
+export default UserList;
